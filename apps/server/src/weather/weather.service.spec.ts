ad4c8f1d15eb3179216a647c35419be81173cf7f
wesleysmyth <wesleytatesmith@gmail.com>
2024-11-11 15:50:40 -0500

    Update weather service spec and add npm scripts

--- apps/server/src/weather/weather.service.spec.ts
+++ apps/server/src/weather/weather.service.spec.ts
@@ -1,51 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpException, HttpStatus } from '@nestjs/common';
-import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
 import { WeatherService } from './weather.service';
-import { Units } from '../common/enums/units.enums';
-import { WeatherData, AIWeatherData } from './interfaces/weather.interface';
-import { mockWeatherData } from '../../__mocks__/weatherDataMock';
+import { AIWeatherData } from './interfaces/weather.interface';
 
 jest.mock('axios');
 
 describe('WeatherService', () => {
     let weatherService: WeatherService;
-    let configService: ConfigService;
 
     const mockApiKey = 'test_api_key';
     const mockOpenAIKey = 'openai_test_key';
-    // const mockWeatherData: WeatherData = {
-    //     lat: 40.7128,
-    //     lon: -74.006,
-    //     timezone: 'America/New_York',
-    //     timezone_offset: -14400,
-    //     current: {
-    //         dt: 1623342340,
-    //         sunrise: 1623318895,
-    //         sunset: 1623369275,
-    //         temp: 298.55,
-    //         feels_like: 298.96,
-    //         pressure: 1013,
-    //         humidity: 78,
-    //         dew_point: 293.55,
-    //         uvi: 0,
-    //         clouds: 20,
-    //         visibility: 10000,
-    //         wind_speed: 5.66,
-    //         wind_deg: 220,
-    //         wind_gust: 8.23,
-    //         weather: [
-    //             {
-    //                 id: 800,
-    //                 main: 'Clear',
-    //                 description: 'clear sky',
-    //                 icon: '01d',
-    //             },
-    //         ],
-    //     },
-    // };
-
     const mockOpenAIResponse = {
         choices: [
             { message: { content: 'Clear skies with mild temperatures.' } },
@@ -70,9 +34,7 @@ describe('WeatherService', () => {
         }).compile();
 
         weatherService = module.get<WeatherService>(WeatherService);
-        configService = module.get<ConfigService>(ConfigService);
 
-        // Mock OpenAI client method
         weatherService['openAIClient'].chat.completions.create = jest
             .fn()
             .mockResolvedValue(mockOpenAIResponse);
